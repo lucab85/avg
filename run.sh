87ae284056f67d761c664fe04e2747e520f5aee2
Luca Berton <lberton@redhat.com>
2021-05-06 17:03:00 +0200

    * CI badge * run.sh/stop.sh scripts

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TAG=latest
+CMD=pptx2ari.sh
 docker pull lucab85/avg:$TAG
 docker run --name="avg" -dit --mount type=bind,source=$(pwd)/share,target=/share --env-file=default.env lucab85/avg:$TAG
 docker ps -a
-docker exec -it "avg" bash
+docker exec -it "avg" $CMD
--- stop.sh
+++ stop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker stop avg
 docker rm avg
-docker rmi lucab85/avg
